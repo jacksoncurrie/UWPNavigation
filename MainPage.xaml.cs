c7e6b7ee753620b826465affce2d10e8f28076fd
Jackson Currie <j.currie1999@gmail.com>
2020-05-09 15:20:59 +1200

    Add navigation with a left hand navigation view

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace NavigationPractice
 {
@@ -24,7 +14,46 @@ namespace NavigationPractice
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            BackStack = new Stack();
+            NavView.SelectedItem = NavView.MenuItems.OfType<NavigationViewItem>().First(n => n.Tag.Equals("home"));
+        }
+
+        private Stack BackStack { get; set; }
+        private readonly List<(string Tag, Type Page)> Pages = new List<(string Tag, Type Page)>
+        {
+            ("home", typeof(Home)),
+            ("calendar", typeof(Calendar))
+        };
+
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to navigate to page.");
+        }
+
+        private void NavigationView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            // Pop off current page
+            BackStack.Pop();
+
+            // Get last page navigated to
+            var selectedItem = BackStack.Pop();
+            NavView.SelectedItem = selectedItem;
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            BackStack.Push(args.SelectedItem);
+
+            // Check if enough pages to go back
+            NavView.IsBackEnabled = BackStack.Count >= 2;
+
+            // Navigate to the current page selected
+            var selectedItem = args.SelectedItem as NavigationViewItem;
+            Type pageToNavigateTo = args.IsSettingsSelected
+                ? typeof(Settings)
+                : Pages.First(p => p.Tag.Equals(selectedItem.Tag)).Page;
+            ContentFrame.Navigate(pageToNavigateTo);
         }
     }
 }
