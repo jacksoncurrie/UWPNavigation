17d12e26f38df5b527a53820b2f198592949b5d9
Jackson Currie <j.currie1999@gmail.com>
2020-05-13 22:28:55 +1200

    Begin implementing CalendarView

--- Calendar.xaml.cs
+++ Calendar.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace NavigationPractice
 {
@@ -24,7 +13,37 @@ namespace NavigationPractice
     {
         public Calendar()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            CalendarViewDayItems = new List<CalendarViewDayItem>();
+        }
+
+        private List<CalendarViewDayItem> CalendarViewDayItems { get; }
+
+        private async void CalendarView_SelectedDatesChanged(CalendarView sender, CalendarViewSelectedDatesChangedEventArgs args)
+        {
+            var cd = new ContentDialog
+            {
+                Content = "Adding Colors to Date",
+                PrimaryButtonText = "Close"
+            };
+
+            foreach (var item in sender.SelectedDates)
+            {
+                var currentDayItem = CalendarViewDayItems.SingleOrDefault(i => i.Date == item);
+                if (currentDayItem != null)
+                {
+                    await cd.ShowAsync();
+                    var densityColors = new List<Color> { Colors.Green, Colors.Blue };
+                    currentDayItem.SetDensityColors(densityColors);
+                }
+            }
+        }
+
+        private void CalendarView_CalendarViewDayItemChanging(CalendarView sender, CalendarViewDayItemChangingEventArgs args)
+        {
+            // Add all the day items loaded
+            if (!CalendarViewDayItems.Contains(args.Item))
+                CalendarViewDayItems.Add(args.Item);
         }
     }
 }
